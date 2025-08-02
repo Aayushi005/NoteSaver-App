439336c80528593d614901feceec3f8b069952f3
Aayushi <harshita05062003@gmail.com>
2025-08-02 23:42:30 +0530

    ViewPaste component modified

--- src/components/ViewPaste.jsx
+++ src/components/ViewPaste.jsx
@@ -8,31 +8,35 @@ const ViewPaste=()=>{
     const paste = allPastes.filter((p)=> p._id ===id)[0];
     console.log(paste);
 
-    return(
-        <div>
-        <div className="flex flex-row  mt-2 gap-5 place-content-between">
-          <input  className="p-2 pl-3 rounded-2xl mt-2 w-[65%]"
-            type="text"
-            placeholder="Enter title here.."
-            value={paste.title}
-            disabled
-            
-          />
-          {/*<button onClick={createPaste} className=" p-2 rounded-4xl">{paste?"Update Paste":"Create My Paste"}</button>*/}
+    return (
+      <div>
+        <div className=" mt-10 ml-2 text-4xl font-bold font-sans pl-3.5">
+          <h1>All Pastes</h1>
         </div>
-       
-            
-        <div className="mt-2 p-2">
-            <textarea className="rounded-2xl mt-2 p-2 min-w-[500px]"
-               value={paste.content}
-               placeholder="Enter content here.. "
-               disabled
-               
-               rows={20}
-               
+
+        <div className="border-2 mt-5 min-w[500px]">
+          <div className="flex flex-row  ml-2 gap-5 rounded-md place-content-between">
+            <input
+              className=" text-3xl   p-2 pl-3 rounded-2xl mt-2 w-[65%]"
+              type="text"
+              placeholder="Enter title here.."
+              value={paste.title}
+              disabled
             />
-        </div>       
-    </div>
-    )
+            {/*<button onClick={createPaste} className=" p-2 rounded-4xl">{paste?"Update Paste":"Create My Paste"}</button>*/}
+          </div>
+
+          <div className="  ml-2 rounded-2xl  p-2">
+            <textarea
+              className="  mt-2 p-2 min-w-[500px]"
+              value={paste.content}
+              placeholder="Enter content here.. "
+              disabled
+              rows={20}
+            />
+          </div>
+        </div>
+      </div>
+    );
 }
 export default ViewPaste
